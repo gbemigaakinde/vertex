03a97b5751fcba596cb796e7e9e3317bff1f53c1
gbemigaakinde <gbemigaakinde@gmail.com>
2026-03-06 20:41:56 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 
 'use strict';
 
-const CACHE_VERSION = 'v1.0.6';
+const CACHE_VERSION = 'v1.1.0';
 const STATIC_CACHE  = `static-${CACHE_VERSION}`;
 const CDN_CACHE     = `cdn-${CACHE_VERSION}`;
 
