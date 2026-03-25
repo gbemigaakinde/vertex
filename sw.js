220f34d7c409be6d609fc0d25a2114d3b883fd3e
gbemigaakinde <gbemigaakinde@gmail.com>
2026-03-25 10:51:28 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -42,7 +42,7 @@ firebase.initializeApp({
 
 const _fcmMessaging = firebase.messaging();
 
-const CACHE_VERSION = 'v1.3.0';
+const CACHE_VERSION = 'v1.3.1';
 const STATIC_CACHE  = `static-${CACHE_VERSION}`;
 const CDN_CACHE     = `cdn-${CACHE_VERSION}`;
 
