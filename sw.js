7b5ab2a80c66329fc590ed219ebf56efb85dd41b
gbemigaakinde <gbemigaakinde@gmail.com>
2026-08-27 08:04:53 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 
 'use strict';
 
-const CACHE_VERSION = 'v1.20.43';
+const CACHE_VERSION = 'v1.20.44';
 const STATIC_CACHE  = `static-${CACHE_VERSION}`;
 const CDN_CACHE     = `cdn-${CACHE_VERSION}`;
 
